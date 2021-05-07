3cff6ea776b1cd50f312d319f191854b63601bc9
Vᴀɪʙʜᴀᴠ <79497577+GodVaibhav@users.noreply.github.com>
2021-05-07 22:00:15 +0530

    Update __main__.py

--- claire/__main__.py
+++ claire/__main__.py
@@ -78,7 +78,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("tg_bot.modules." + module_name)
+    imported_module = importlib.import_module("claire.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
