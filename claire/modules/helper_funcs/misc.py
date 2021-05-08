06c29325e831ffcbe2ff4e3a4b8cbef4b777366e
Vᴀɪʙʜᴀᴠ <79497577+GodVaibhav@users.noreply.github.com>
2021-05-08 09:56:43 +0530

    Update misc.py

--- claire/modules/helper_funcs/misc.py
+++ claire/modules/helper_funcs/misc.py
@@ -1,13 +1,10 @@
-from math import ceil
-from typing import List, Dict
-
-from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
+from claire import NO_LOAD
+from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
-
-from claire import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
+
     def __eq__(self, other):
         return self.text == other.text
 
@@ -41,40 +38,48 @@ def split_message(msg: str) -> List[str]:
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-        modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(
+                x.__mod_name__,
+                callback_data="{}_module({})".format(prefix,
+                                                     x.__mod_name__.lower()))
+            for x in module_dict.values()
+        ])
     else:
-        modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(
+                x.__mod_name__,
+                callback_data="{}_module({},{})".format(prefix, chat,
+                                                        x.__mod_name__.lower()))
+            for x in module_dict.values()
+        ])
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = [
+        modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
+    ]
 
-    if len(modules) % 2 == 1:
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
         pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
+    elif calc == 2:
+        pairs.append((modules[-1],))
 
     return pairs
 
 
-def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
+def send_to_list(bot: Bot,
+                 send_to: list,
+                 message: str,
+                 markdown=False,
+                 html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
         try:
             if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
+                bot.send_message(
+                    user_id, message, parse_mode=ParseMode.MARKDOWN)
             elif html:
                 bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
             else:
@@ -105,5 +110,18 @@ def revert_buttons(buttons):
     return res
 
 
+def build_keyboard_parser(bot, chat_id, buttons):
+    keyb = []
+    for btn in buttons:
+        if btn.url == "{rules}":
+            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
+        if btn.same_line and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
+        else:
+            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
 def is_module_loaded(name):
-    return (not LOAD or name in LOAD) and name not in NO_LOAD
+    return name not in NO_LOAD
