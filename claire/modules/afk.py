ad94f613cfb450c7620222b9ffa7738d213173cc
Vᴀɪʙʜᴀᴠ <79497577+GodVaibhav@users.noreply.github.com>
2021-05-08 09:51:21 +0530

    Update afk.py

--- claire/modules/afk.py
+++ claire/modules/afk.py
@@ -4,10 +4,10 @@ from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
-from tg_bot.modules.sql import afk_sql as sql
-from tg_bot.modules.users import get_user_id
+from claire import dispatcher
+from claire.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from claire.modules.sql import afk_sql as sql
+from claire.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
