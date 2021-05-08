1d3edcf188a820a8c5132b0b370c5e6e77452d06
Vᴀɪʙʜᴀᴠ <79497577+GodVaibhav@users.noreply.github.com>
2021-05-08 10:30:18 +0530

    Update __main__.py

--- claire/__main__.py
+++ claire/__main__.py
@@ -56,7 +56,7 @@ Hello! My name is *{}*.
 I'm a modular group management bot with a few fun extras!
 Have a look at the following for an idea of some of the things I can help you with.
 
-*Helpful Commands:*:
+*Helpful Commands:*
  ✪ /start: Starts Me!, You have have probably done this!
  ✪ /help: I'll tell you myself about this!
  
